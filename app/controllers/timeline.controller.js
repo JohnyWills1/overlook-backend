6489e36fe841e4d32b1d3f79aeb2ae35f5a197c3
Johny Wills <jonathan_wills@icloud.com>
2020-10-20 18:22:56 +0100

    added simple CRUD for timeline model

--- app/controllers/timeline.controller.js
+++ app/controllers/timeline.controller.js
@@ -31,19 +31,95 @@ exports.create = (req, res) => {
 };
 
 // Find all Timelines from the database
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+	const title = req.query.title;
+
+	// Create a condition to search by or just return all the timelines
+	var condition = title
+		? { title: { $regex: new RegExp(title), $options: 'i' } }
+		: {};
+
+	Timeline.find(condition)
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message || 'Some error occured while retrieving Timelines.',
+			});
+		});
+};
 
 // Find a single Timeline with an ID
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+	const id = req.params.id;
+
+	Timeline.findById(id)
+		.then((data) => {
+			!data
+				? res.status(404).send({ message: 'Not found Timeline with id ' + id })
+				: res.send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message || 'Error retrieving Timeline with id=' + id,
+			});
+		});
+};
 
 // Update a Timeline by the ID in the request
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+	if (!req.body) {
+		return res.status(400).send({
+			message: 'Data to update cannot be empty!',
+		});
+	}
+
+	const id = req.params.id;
+
+	Timeline.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+		.then((data) => {
+			!data
+				? res.status(404).send({
+						message: `Cannot update Timeline with id=${id}. Maybe the Timeline was not found!`,
+				  })
+				: res.send({ message: 'Timeline was updated successfully.' });
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: 'Error updating Timeline with id=' + id,
+			});
+		});
+};
 
 // Delete a Timeline with the specified ID in the request
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+	const id = req.params.id;
 
-// Delete all Timelines from the database
-exports.deleteAll = (req, res) => {};
+	Timeline.findByIdAndDelete(id)
+		.then((data) => {
+			!data
+				? res.status(404).send({
+						message: `Cannot delete Timeline with id=${id}. Maybe Timeline was not found!`,
+				  })
+				: res.send({ message: 'Timeline was deleted successfully.' });
+		})
+		.catch((err) => {
+			res.send(500).send({
+				message: 'Could not delete the Timeline with id=' + id,
+			});
+		});
+};
 
 // Find all published Timelines
-exports.findAllPublished = (req, res) => {};
+exports.findAllPublished = (req, res) => {
+	Timeline.find({ published: true })
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message || 'Some error occurred while retrieving Timelines.',
+			});
+		});
+};
--- app/routes/timeline.routes.js
+++ app/routes/timeline.routes.js
@@ -1,10 +1,26 @@
 module.exports = (app) => {
+	// Import the timeline controller - giving us access to all the functions
 	const timelines = require('../controllers/timeline.controller');
 
 	var router = require('express').Router();
 
+	// Find all published Timelines
+	router.get('/published', timelines.findAllPublished);
+
 	// Create a new Timeline
 	router.post('/', timelines.create);
+
+	// Find All timlines in the DB
+	router.get('/', timelines.findAll);
+
+	// Find a single Timeline with the ID defined
+	router.get('/:id', timelines.findOne);
+
+	// Update a specific Timeline with a given ID
+	router.put('/:id', timelines.update);
+
+	// Delete a specific Timeline with a given ID
+	router.delete('/:id', timelines.delete);
 
 	app.use('/api/timelines', router);
 };
